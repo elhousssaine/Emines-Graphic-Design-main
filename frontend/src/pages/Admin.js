6ae3e11423ec0b64d77f8f3ed435146365582ad5
elhousssaine <124269848+elhousssaine@users.noreply.github.com>
2023-06-10 00:49:54 +0100

    Admin.js

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -87,7 +87,7 @@ const Admin = () => {
         .catch((err) => {
           console.log(err);
         });
-      setProjects(resp);
+        setProjects(resp);
 
         const resp2 = await apiClient
         .get(api_url_blog)
