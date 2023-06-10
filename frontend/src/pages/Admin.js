6063a732ada2860c5380c2465bfc3e3dc38276d7
elhousssaine <124269848+elhousssaine@users.noreply.github.com>
2023-06-10 19:27:19 +0100

    Admin.js

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -78,11 +78,13 @@ const Admin = () => {
   const getProject = async () => {
       setLoading(true);
       try {
-        const resp = await apiClient.get("/api/projects/getbyowner/" + userInfo.id);
-        setProjects(resp.data);
 
         const resp2 = await apiClient.get("/api/blogs/getbyowner/" + userInfo.id);
         setBlogs(resp2.data);
+        
+        const resp = await apiClient.get("/api/projects/getbyowner/" + userInfo.id);
+        setProjects(resp.data);
+        
       } catch (error) {
         console.error('Error fetching project:', error);
         // Handle the error appropriately
