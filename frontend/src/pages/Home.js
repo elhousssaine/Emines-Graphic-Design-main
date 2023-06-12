543a5decb5e4f8a33137d76d11288a2bbff6aebe
elhousssaine <124269848+elhousssaine@users.noreply.github.com>
2023-06-12 14:53:30 +0100

    Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -34,13 +34,35 @@ function Home() {
         <div className="col subtitle__RecContent">Our vibrant community of university students, backed by UM6P's state-of-the-art facilities, delivers a dynamic blend of youth and innovation. With numbers on our side, we bring a diverse range of ideas and perspectives to the table. From captivating graphics to cutting-edge websites, we harness the power of collaboration and unleash unparalleled creativity. Join us and experience a new era of design excellence that sets your brand apart from the rest. Get ready to redefine the game together!</div>
       </div>
       </div>
-        <ContentRec2 key="content-rec-2" title="" />
+        <ContentRec5 key="content-rec-5" title="" />
         <Blackbords />
         <MyComponent />
-        <ContentRec5 key="content-rec-5" title="" />
+                       <div className="feed-grid">
+              <div className="RecContentf  full-height d-flex flex-column">
+            <img src="/public/images/d5b5fd2b-3aa8-4a92-9b0f-1bf09fd9bddc-1685999424447.png" alt="a balloon" class="myimg"></img><div className="row title__RecContentf">Fresh Alternative To Traditional Agencies</div>
+      <div className="row mt-auto">
+        <div className="col subtitle__RecContentf">Our vibrant community of university students, backed by UM6P's state-of-the-art facilities, delivers a dynamic blend of youth and innovation.</div>
       </div>
+      </div>
+      <div className="RecContentf  full-height d-flex flex-column">
+            <img src="/public/images/8511afcb-4125-47ff-8959-051de290d75c-1685999386255.png" alt="a balloon" class="myimg"></img><div className="row title__RecContentf">Fresh Alternative To Traditional Agencies</div>
+      <div className="row mt-auto">
+        <div className="col subtitle__RecContentf">Our vibrant community of university students, backed by UM6P's state-of-the-art facilities, delivers a dynamic blend of youth and innovation.</div>
+      </div>
+      </div>
+      <div className="RecContentf  full-height d-flex flex-column">
+            <img src="/public/images/d5b5fd2b-3aa8-4a92-9b0f-1bf09fd9bddc-1685999424447.png" alt="a balloon" class="myimg"></img><div className="row title__RecContentf">Fresh Alternative To Traditional Agencies</div>
+      <div className="row mt-auto">
+        <div className="col subtitle__RecContentf">Our vibrant community of university students, backed by UM6P's state-of-the-art facilities, delivers a dynamic blend of youth and innovation.</div>
+      </div>
+      </div>
+      </div>
+        
+      </div>
+      <div className="RecContentportfolio  full-height d-flex flex-row">
+      <div className="row title__RecContentportfolio">More Projects</div><div className="col subtitle__RecContentportfolio">Discover our stunning portfolio, a testament to our design prowess.</div></div>
       <div className="MainRec">
-                <footer className="App-footer d-flex flex-wrap mb-3 justify-content-between" id="footer_section_id">
+                <footer className="App-footer  flex-wrap mb-3 justify-content-between" id="footer_section_id">
   <div className="footer-contact">
     <Contact key="cntct" />
   </div>
