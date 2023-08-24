e404df67a8a18bf99433f4d595159743dce6b453
elhousssaine <124269848+elhousssaine@users.noreply.github.com>
2023-08-24 13:27:53 +0100

    Homex.js

--- frontend/src/pages/Homex.js
+++ frontend/src/pages/Homex.js
@@ -23,9 +23,7 @@ function Udc() {
       Our vibrant community of university students, backed by UM6P's state-of-the-art facilities, delivers a dynamic blend of youth and innovation. <br /> With numbers on our side, we bring a diverse range of ideas and perspectives to the table. From captivating graphics to cutting-edge websites, we harness the power of collaboration and unleash unparalleled creativity. Join us and experience a new era of design excellence . <br /><br /><br />
     <NavLink
   to="/portfolio"
-  id="LogoText"
-  className="navbar-brand"
-  style={{ color: light ? "#000000" : "white" }}
+  id="Portfoliolink"
 >
   Check out our portfolio
 </NavLink>  &nbsp;&nbsp;  &nbsp;&nbsp; <a className="link"  href="/Home">Hire us</a>.
